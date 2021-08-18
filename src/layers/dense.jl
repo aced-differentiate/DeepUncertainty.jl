86d8d6e3db8eb963b06abd10ef44ccb92f60d71c
DwaraknathT <dwarakasharma@gmail.com>
2021-08-18 18:30:22 +0530

    DenseBatchEnsemble ranks merged

--- src/layers/dense.jl
+++ src/layers/dense.jl
@@ -1,21 +1,21 @@
 using Flux 
-using Flux:@functor, glorot_uniform, create_bias
+using Flux:@functor, glorot_uniform, create_bias, batch, reshape
 
-struct DenseBatchEnsemble{L,F,M <: AbstractMatrix,B}
+struct DenseBatchEnsemble{L,F,M,B}
     layer::L 
-    α::M
-    γ::M
+    alpha::M
+    gamma::M
     ensemble_bias::B
-    ensemble_σ::F
+    ensemble_act::F
     rank::Integer
-    function DenseBatchEnsemble(layer::L, α::M, γ::M, 
+    function DenseBatchEnsemble(layer::L, alpha::M, gamma::M, 
                                 ensemble_bias=true, 
-                                ensemble_σ::F=identity, 
-                                rank::Integer=1) where {M <: AbstractMatrix,F,L}
-        ensemble_bias = create_bias(γ, ensemble_bias, size(γ)[1], size(γ)[2])
-        new{typeof(layer),F,M,typeof(ensemble_bias)}(layer, α, γ, 
+                                ensemble_act::F=identity, 
+                                rank::Integer=1) where {M,F,L}
+        ensemble_bias = create_bias(gamma, ensemble_bias, size(gamma)[1], size(gamma)[2])
+        new{typeof(layer),F,M,typeof(ensemble_bias)}(layer, alpha, gamma, 
                                                     ensemble_bias, 
-                                                    ensemble_σ, 
+                                                    ensemble_act, 
                                                     rank)
     end
 end
@@ -26,48 +26,50 @@ function DenseBatchEnsemble(in::Integer, out::Integer,
                             init=glorot_uniform, 
                             bias=true)
     layer = Flux.Dense(in, out; init=init, bias=bias)
-    if rank > 1 
-        α_shape = (in, ensemble_size, rank)
-        γ_shape = (out, ensemble_size, rank)
-    elseif rank == 1 
-        α_shape = (in, ensemble_size)
-        γ_shape = (out, ensemble_size) 
+    if rank >= 1 
+        alpha_shape = (in, ensemble_size, rank)
+        gamma_shape = (out, ensemble_size, rank)
     else
         error("Rank must be >= 1.")
     end 
-    α = init(α_shape) 
-    γ = init(γ_shape)
+    alpha = init(alpha_shape) 
+    gamma = init(gamma_shape)
   
-    return DenseBatchEnsemble(layer, α, γ, bias, σ, rank)
+    return DenseBatchEnsemble(layer, alpha, gamma, bias, σ, rank)
 end
   
 @functor DenseBatchEnsemble
   
 function (a::DenseBatchEnsemble)(x::AbstractVecOrMat)
     layer = a.layer 
-    α = a.α
-    γ = a.γ
+    alpha = a.alpha
+    gamma = a.gamma
     e_b = a.ensemble_bias 
-    e_σ = a.ensemble_σ
+    e_σ = a.ensemble_act
     rank = a.rank
 
     batch_size = size(x)[end]
-    in_size = size(α)[1]
-    out_size = size(γ)[1]
-    ensemble_size = size(α)[2]
+    in_size = size(alpha)[1]
+    out_size = size(gamma)[1]
+    ensemble_size = size(alpha)[2]
     samples_per_model = batch_size ÷ ensemble_size 
 
-    # TODO: Implement Rank > 1 computations 
-    # Reshape the inputs, α and γ
-    x = reshape(x, (in_size, samples_per_model, ensemble_size))
-    α = reshape(α, (in_size, 1, ensemble_size))
-    γ = reshape(γ, (out_size, 1, ensemble_size))
-    e_b = reshape(e_b, (out_size, 1, ensemble_size))
+    # Alpha, gamma shapes - [units, ensembles, rank]
+    alpha = repeat(alpha, outer=[samples_per_model, 1, 1])
+    gamma = repeat(gamma, outer=[samples_per_model, 1, 1])
+    # Reshape alpha, gamma to [units, batch_size, rank]
+    alpha = reshape(alpha, (in_size, batch_size, rank))
+    gamma = reshape(gamma, (out_size, batch_size, rank))
+    x = reshape(x, (in_size, batch_size, 1))
     # Perturb the inputs 
-    perturbed_x = x .* α;
+    perturbed_x = x .* alpha;
     # Dense layer forward pass 
-    outputs = layer(perturbed_x)
-    outputs = outputs .* γ
+    outputs = layer(perturbed_x) .* gamma
+    # Reduce the rank dimension through summing it up
+    outputs = sum(outputs, dims=3)
+    outputs = reshape(outputs, (out_size, samples_per_model, ensemble_size))
+    # Reshape ensemble bias 
+    e_b = reshape(e_b, (out_size, 1, ensemble_size))
     outputs = e_σ(outputs .+ e_b)
     outputs = reshape(outputs, (out_size, batch_size))
     return outputs
@@ -77,8 +79,8 @@ end
     reshape(a(reshape(x, size(x, 1), :)), :, size(x)[2:end]...)
   
 function Base.show(io::IO, l::DenseBatchEnsemble)
-    print(io, "DenseBatchEnsemble(", size(l.weight, 2), ", ", size(l.weight, 1))
-    l.σ == identity || print(io, ", ", l.σ)
-    l.bias == Zeros() && print(io, "; bias=false")
+    print(io, "DenseBatchEnsemble(", size(l.layer.weight, 2), ", ", size(l.layer.weight, 1))
+    l.layer.σ == identity || print(io, ", ", l.layer.σ)
+    l.layer.bias == Zeros() && print(io, "; bias=false")
     print(io, ")")
 end
