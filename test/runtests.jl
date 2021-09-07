ab89d825b99f5958b6098ace76a27af90c4b45c9
Dwaraknath Gnaneshwar <34276428+DwaraknathT@users.noreply.github.com>
2021-09-07 16:53:30 +0530

    Revert "Added Dense and Conv BatchEnsemble layers along with unit tests and example on MNIST classification using LeNet5 (#4)" (#14)
    This reverts commit ed3d16c970353bf03271febcf7f020c4934f0862.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,7 @@
 using DeepUncertainty
 using Test
-using Flux
-using Flux.CUDA
-using Flux: cpu, gpu
 
 @testset "Layers" begin
-    # MC layers 
-    include("./layers/mclayers.jl")
-    # Batch ensembe layers 
-    include("./layers/batchensemble.jl")
-end
-
-@testset "CUDA" begin
-    if CUDA.functional()
-        include("cuda/runtests.jl")
-    else
-        @warn "CUDA unavailable, not testing GPU support"
-    end
+    # Test the layers 
+    include("./layers/mclayers_test.jl")
 end
